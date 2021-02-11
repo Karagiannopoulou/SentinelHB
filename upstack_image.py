2c52d92c29ee3d5e0dfd7e05ad6ec144fa695c74
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2021-02-11 18:27:19 +0200

    Update upstack_image.py

--- upstack_image.py
+++ upstack_image.py
@@ -1,38 +1,55 @@
 # Default libs
-import sys
-import os
+import sys, os
 
 # Basics of GIS
 from osgeo import gdal
+
+# Global vars
 mainDirectory = r'.\COG'
 
-def unstack_image(input_path):
-    for root, dirs, files in os.walk(input_path, topdown=True):  
-        for dir in dirs:
-            full_path = os.path.join(input_path, dir)
-            for file in os.listdir(full_path):
-                filename = os.fsdecode(file)
-                if filename.endswith(".tif"):                          
-                    filename_path = os.path.join(full_path, filename)
-                    name = os.path.splitext(os.path.basename(filename_path))[0]
+def split(word):
+    n=2 
+    if (len(word)%2)==1: # odd length if the bands are 3 (5,6,7)
+        return [char for char in word]
+    elif (len(word)%2)==0:  # even length if the bands are 6 (8a,11,12)
+        return [word[index : index + n] for index in range(0, len(word), n)]
+            
 
-                    tile_folder = os.path.join(full_path,name) # create folders from the name of the images
-                                            
-                    if not(os.path.exists(tile_folder) and os.path.isdir(tile_folder)):
-                        os.makedirs(tile_folder)
-
-                        img = gdal.Open(filename_path)
-                        for band in range(img.RasterCount): #Save bands as individual files
-                            band = band+1
-                            outputband = os.path.join(tile_folder,f'B{band}.tiff')
-                            print(outputband)
-                            gdal.Translate(outputband, img, format='GTiff', bandList=[band])
-                            
-                                
-                                           
+def unstack_image(input_path, subfolder_prefix):
+    for _, dirs,_ in os.walk(input_path):  
+        for folder in dirs:
+            full_path = os.path.join(input_path, folder)
+            if subfolder_prefix in full_path:
+                for file in os.listdir(full_path):
+                    file_name = os.fsdecode(file) # decode filesystem
+                    if file_name.endswith(".tif"):                          
+                        filename_path = os.path.join(full_path, file_name)
+                        name = os.path.splitext(os.path.basename(filename_path))[0] 
+                        img_date = name.split("_")[-1] # take the time of the image
+                        rgbName = name.split("_")[1] # take the rgb of the image
+                        rgbBand_list = split(rgbName)
+    
+                        if (len(rgbName)%2)==1 and rgbName in filename_path:
+                            tile_folder = os.path.join(full_path,img_date) # create folders from the name of the images                                
+                            if not os.path.exists(tile_folder):
+                                os.makedirs(tile_folder)
+                                img = gdal.Open(filename_path)
+                                for band,b in zip(range(img.RasterCount),rgbBand_list): 
+                                    band = band+1
+                                    outputband = os.path.join(tile_folder,f'B{b}.tiff')
+                                    gdal.Translate(outputband, img, format='GTiff', bandList=[band])
+                                    print(outputband)
                         
-
+                        if (len(rgbName)%2)==0 and rgbName in filename_path:
+                            if os.path.exists(tile_folder):
+                                
+                                img = gdal.Open(filename_path)
+                                for band,b in zip(range(img.RasterCount),rgbBand_list): 
+                                    band = band+1                                     
+                                    outputband = os.path.join(tile_folder,f'B{b}.tiff')
+                                    gdal.Translate(outputband, img, format='GTiff', bandList=[band])
+                                    print(outputband)
 
 if __name__ == '__main__':
-    unstack_image(mainDirectory)
+    unstack_image(mainDirectory, 'tile')
     
