bea6967b5e60aaa453ec431227700a1df80a51fa
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2021-04-06 21:30:13 +0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -22,8 +22,8 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
 # Your client credentials
-client_id = ''
-client_secret = ''
+client_id = '629e3c27-b93e-4990-83d5-106707ffff1b'
+client_secret = 's}lwWt}EUC6Irw0D7H[Cf5Q[G[]QT51aAg6|8W#%'
 
 # Fetch an access token from the provider
 client = BackendApplicationClient(client_id=client_id)
