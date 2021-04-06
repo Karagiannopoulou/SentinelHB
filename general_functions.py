893c1c5a3933ee1163c5061df2e4726822ae7191
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2021-04-06 21:29:49 +0300

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -5,15 +5,13 @@ def makepath(path):
         os.makedirs(path)
     
     return path
-        
+
 def cleanFolder(mainroot, subfolder_prefix):
-    
     for folder in os.listdir(mainroot):
         if folder.startswith(subfolder_prefix):
             path = os.path.join(mainroot, folder)
             for subfolder in os.listdir(path):
                 subfolderpath = os.path.join(path, subfolder)
-                print(subfolderpath)
                 if os.path.isfile(subfolderpath):
                     os.remove(subfolderpath)
                 else:
