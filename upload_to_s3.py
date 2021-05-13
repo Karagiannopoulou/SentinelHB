1b73f647ae55a6faee7d380552ea60d87b227bdb
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2021-05-13 15:26:19 +0300

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -2,12 +2,10 @@ import os, sys
 from secrets import aws_access_key_id, aws_secret_access_key, dione_s3_bucket
 import boto3
 
-main_Directory = r'D:\DIONE\WP3\SuperResolution\uploadData'
-
 
 def upload_to_s3(local_imagefile, keyname):
 
-    imageFile = open(local_imagefile, 'rb') # open and read the locally stored image file 
+    imageFile = open(local_imagefile, 'rb') # open and read the locally stored image 
     bucket_folder = 'cogs/tiles-iccs'
     key_filename = "{}/{}".format(bucket_folder,keyname)      
     s3 = boto3.client(
@@ -20,30 +18,16 @@ def upload_to_s3(local_imagefile, keyname):
               Key=key_filename,
               Body=imageFile)
     
-    print(key_filename)
-    
-    
-
-def ingest_to_S3(input_path):
-    
-    for folder in os.listdir(input_path):
-        full_path = os.path.join(input_path, folder)
-        for subfolder in os.listdir(full_path):
-            subfolder_fullpath = os.path.join(full_path, subfolder)
-            for cogsFolder in os.listdir(subfolder_fullpath):
-                cogsFolder_fullpath = os.path.join(subfolder_fullpath, cogsFolder)  
-                if os.path.isdir(cogsFolder_fullpath):         
-                    for ffile in os.listdir(cogsFolder_fullpath):
-                        file_name = os.fsdecode(ffile) # decode file system
-                        if file_name.endswith('cog.tiff'):
-                            filename_path = os.path.join(cogsFolder_fullpath, file_name)
-                            target = filename_path.split("\\")[-4:] # take the three last components of the local path to define the s3 tile
-                            key_target = os.path.join(*target)
-                            backslash_key_target = key_target.replace(os.sep, '/') # change the slashes to adapt to AWS file system style
-                            print(backslash_key_target)
-                            upload_to_s3(filename_path, backslash_key_target)
-            
+    return key_filename
 
 
-if __name__ == '__main__':
-    ingest_to_S3(main_Directory)
+def ingest_to_S3(ffile):
+    filename_path = os.fsdecode(ffile) # decode file system because it contains only numbers
+    if filename_path.endswith('cog.tiff'):
+        target = filename_path.split("\\")[-4:] # take the three last components of the local path to define the s3 tile
+        key_target = os.path.join(*target)
+        backslash_key_target = key_target.replace(os.sep, '/') # change the slashes to adapt to AWS file system style
+        ingested_image = upload_to_s3(filename_path, backslash_key_target)
+        
+        return ingested_image
+
